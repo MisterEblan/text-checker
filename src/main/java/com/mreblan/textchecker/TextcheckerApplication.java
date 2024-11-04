9ed67271114dbb22fec688c433e1b0e60ea87fee
Danil <boy4rov.da@gmail.com>
2024-11-04 16:11:28 +0300

    first commit

---
+++ src/main/java/com/mreblan/textchecker/TextcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.mreblan.textchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextcheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mreblan/textchecker/TextcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mreblan.textchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
